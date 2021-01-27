0278c26c788ef91f23c9a73cc4e3afafc89b89e1
mpitavidal <mpitavidal@gmail.com>
2021-01-27 12:38:28 +0100

    Update daily_updates.py

--- daily_updates.py
+++ daily_updates.py
@@ -10,16 +10,17 @@ def do_one_run():
     TODO: Add docstring
     """
     print("Updating")
-    database_upd.database_upd(query_input='search_query_list.txt', database_output="data_lastday.pkl")  # Update database with latest relevant arXiv papers
+    # Update database with latest relevant arXiv papers
+    database_upd.database_upd(query_input='search_query_list.txt', 
+                              database_output="data_lastday.pkl")
     msbot.send_msteams_update(database="data_lastday.pkl")
     # telegram_bot.send_telegram_update()
 
 
-# schedule.every().day.at("12:34").do(do_one_run)
-schedule.every(1).minutes.do(do_one_run)
+schedule.every().day.at("12:34").do(do_one_run)
+# schedule.every(1).minutes.do(do_one_run)
 
 
 while True:
     schedule.run_pending()
     time.sleep(120)
-
