9ff78874d7240bd932a3fc99c885548b7ac39202
mpitavidal <mpitavidal@gmail.com>
2021-01-27 12:45:06 +0100

    Add prints

--- daily_updates.py
+++ daily_updates.py
@@ -13,12 +13,15 @@ def do_one_run():
     # Update database with latest relevant arXiv papers
     database_upd.database_upd(query_input='search_query_list.txt', 
                               database_output="data_lastday.pkl")
+    print("Database updated")
     msbot.send_msteams_update(database="data_lastday.pkl")
+    print("Sent message to Teams")
     post_on_tg.post_the_message()
+    print("Sent message to Telegram")
 
 
-schedule.every().day.at("12:34").do(do_one_run)
-# schedule.every(1).minutes.do(do_one_run)
+# schedule.every().day.at("12:41").do(do_one_run)
+schedule.every(1).minutes.do(do_one_run)
 
 
 while True:
