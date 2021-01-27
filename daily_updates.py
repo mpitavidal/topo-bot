1b7e263f669bf1012c99c2cee00b02c191893d19
mpitavidal <mpitavidal@gmail.com>
2021-01-27 14:05:25 +0100

    Fixed time print

--- daily_updates.py
+++ daily_updates.py
@@ -13,11 +13,11 @@ def do_one_run():
     print("Started updating...")
     database_upd.database_upd(query_input='search_query_list.txt', 
                               database_output="data_lastday.pkl")
-    print("Database updated at {}".format(time.time))
+    print("Database updated at {}".format(time.time()))
     msbot.send_msteams_update(database="data_lastday.pkl")
-    print("Sent message to Teams at {}".format(time.time))
+    print("Sent message to Teams at {}".format(time.time()))
     post_on_tg.post_the_message(database="data_lastday.pkl")
-    print("Sent message to Telegram at {}".format(time.time))
+    print("Sent message to Telegram at {}".format(time.time()))
 
 
 # schedule.every().day.at("12:41").do(do_one_run)
