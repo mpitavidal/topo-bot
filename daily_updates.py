00a76b78b60e0d3ff42638132210d5abbc7fd770
mpitavidal <mpitavidal@gmail.com>
2021-01-27 12:25:42 +0100

    Minor

--- daily_updates.py
+++ daily_updates.py
@@ -1,13 +1,14 @@
 import schedule
 import time
-import msbot
+from msbot import msbot
+# from telegram_bot import ???
 import database_upd
 
-schedule.every().day.at("10:10").do(do_one_run())
+schedule.every().day.at("12:26").do(do_one_run())
 
 
 def do_one_run():
     database_upd.database_upd()  # Update database with latest relevant arXiv papers
     msbot.send_msteams_update()
-    telegram_bot.send_telegram_update()
+    # telegram_bot.send_telegram_update()
     time.sleep(120)
