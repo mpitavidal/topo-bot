1f344919e1c837b3fb4705d3783becb0944a423d
mpitavidal <mpitavidal@gmail.com>
2021-01-27 08:35:14 +0100

    Putting relevant functions from the testing notebook into relevant files

--- msbot/msbot.py
+++ msbot/msbot.py
@@ -1,2 +1,28 @@
-import numpy as np
-import pymsteams
+def fill_message(paper, message):
+    """
+    .......
+
+    Parameters
+    ----------
+    paper : dictionary
+        ----
+    message : pymsteams.connectorcard
+        -----
+
+    Doesn't return anything.
+    """
+    title = paper["title"]
+    text = ""
+    for key in paper:
+        text += (key + ": " + paper[key] + "\n")
+    button_text = "Go to paper in arXiv"
+    paper_link = paper["link"]
+    color = "#40E0D0"
+
+    # TODO: add activity elements for authors info
+    
+    # Add text to the message.
+    message.title(title)
+    message.text(f'<pre>\n{text}\n</pre>')
+    message.addLinkButton(button_text, paper_link)
+    message.color(color)
