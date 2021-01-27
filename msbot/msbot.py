fa0392303f3042d728388558669e93c65e5f290b
mpitavidal <mpitavidal@gmail.com>
2021-01-27 14:33:46 +0100

    Added docstrings

--- msbot/msbot.py
+++ msbot/msbot.py
@@ -9,9 +9,21 @@ ADDRESS_ARXIV = "https://tud365.webhook.office.com/webhookb2/b30f04eb-e52a-4b70-
 
 def send_msteams_update(database=DATABASE, address=ADDRESS_ARXIV):
     """
-    TODO: add docstring
-    """
+    For each of the papers in a database, sends a message to a teams channel
+    containing information about the paper.
 
+    Parameters
+    ----------
+    database : str
+        File containing the database.
+
+    address : str
+        Address to the Microsoft Webhook for the channel of interest.
+
+    Doesn't return anything.
+
+    """
+    
     data_lastday = pd.read_pickle(database)
 
     n_papers = len(data_lastday["title"])   # Number of papers in the dataframe
