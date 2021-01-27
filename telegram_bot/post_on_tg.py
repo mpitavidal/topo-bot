a201588bde0921f11f693a476e45e62847c0cf2b
Tess Vakhtel <77330800+scp062@users.noreply.github.com>
2021-01-27 12:37:06 +0100

    added post_on_tg.py, the function posting the message is called post_the_message

---
+++ telegram_bot/post_on_tg.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[30]:
+
+
+import telegram 
+import pandas as pd
+from telegram.ext import Updater
+def post_the_message():
+    updater = Updater(token='1598945607:AAGpjZu4t7zh4Mok6baddPQ9KxZZ2ngnCbk', use_context=True)
+    dispatcher = updater.dispatcher
+    import logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                         level=logging.INFO)
+    bot = telegram.Bot(token='1598945607:AAGpjZu4t7zh4Mok6baddPQ9KxZZ2ngnCbk')
+    database = '..\data_lastday.pkl'
+    data_lastday = pd.read_pickle(database)
+    author_list=data_lastday['author_list']
+    titles=data_lastday['title']
+    published = data_lastday['published']
+    links=data_lastday['link']
+    ids=data_lastday['id']
+    arxiv_primary_categories=data_lastday['arxiv_primary_category']
+    telegram_text=" "
+    for i in list(ids):
+        telegram_text=telegram_text+ published[i].strftime("%m/%d/%Y")+ "\n \n"+"'"+ titles[i]+ "'" +"\n \n"+author_list[i]+"\n"+links[i]+"\n"+arxiv_primary_categories[i] +"\n \n \n"
+        bot.sendMessage(chat_id='@openmajoranachannel', text=telegram_text)
+    return 
+
