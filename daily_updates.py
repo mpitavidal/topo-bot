1a7bcd52897d1d7ce3780d2fd75200544b514121
mpitavidal <mpitavidal@gmail.com>
2021-01-27 12:27:05 +0100

    Typos

--- daily_updates.py
+++ daily_updates.py
@@ -4,11 +4,15 @@ from msbot import msbot
 # from telegram_bot import ???
 import database_upd
 
-schedule.every().day.at("12:26").do(do_one_run())
-
 
 def do_one_run():
+    """
+    TODO: Add docstring
+    """
     database_upd.database_upd()  # Update database with latest relevant arXiv papers
     msbot.send_msteams_update()
     # telegram_bot.send_telegram_update()
     time.sleep(120)
+
+schedule.every().day.at("12:26").do(do_one_run())
+
